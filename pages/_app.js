dd08f5f45d48627b5831c1bf370f99b2cf45cdb6
Mario Montero <marioupc3@gmail.com>
2022-03-25 11:10:47 -0500

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -30,21 +30,10 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
         />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
-          sizes="32x32"
-          href="/img/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
+          href="/favicon-16x16.png"
           sizes="16x16"
-          href="/img/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest"></link>
         <link href="/fonts/style.css" rel="stylesheet" />
